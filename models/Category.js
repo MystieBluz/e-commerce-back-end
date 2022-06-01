ee382333e8d41bcf1091d5ee7f3721e539c16828
Misty Duhart <mystie.smith@gmail.com>
2022-06-01 13:04:52 -0400

    corrected files

--- models/Category.js
+++ models/Category.js
@@ -4,23 +4,17 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init({
- id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
+Category.init(
+  {
+    // define columns
   },
-  category_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
- }, {
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-});
+  }
+);
 
 module.exports = Category;
